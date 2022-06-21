c997fe52b7ffefa187043a7f7187e9ef18a49c26
Bekarion <beqarioni@yahoo.co.uk>
2022-06-21 09:20:48 +0400

    home->work 6/21/2022

---
+++ controllers/auth.js
@@ -0,0 +1,236 @@
+const crypto = require("crypto")
+
+const bcrypt = require("bcryptjs")
+const nodemailer = require("nodemailer")
+const { validationResult } = require("express-validator")
+
+const User = require("../models/user")
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "d68b050bbff536",
+    pass: "31d67e0a8e910a",
+  },
+})
+
+exports.getLogin = (req, res, next) => {
+  let message = req.flash("error")
+  if (message.length > 0) {
+    message = message[0]
+  } else {
+    message = null
+  }
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    errorMessage: message,
+  })
+}
+
+exports.getSignup = (req, res, next) => {
+  let message = req.flash("error")
+  if (message.length > 0) {
+    message = message[0]
+  } else {
+    message = null
+  }
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    errorMessage: message,
+    oldInput: {
+      email: "",
+      password: "",
+      confirmPassword: req.body.confirmPassword,
+    },
+  })
+}
+
+exports.postLogin = (req, res, next) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/login", {
+      path: "/login",
+      pageTitle: "Login",
+      errorMessage: errors.array()[0].msg,
+    })
+  }
+
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        req.flash("error", "Invalid email or password.")
+        return res.redirect("/login")
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.isLoggedIn = true
+            req.session.user = user
+            return req.session.save((err) => {
+              console.log(err)
+              res.redirect("/")
+            })
+          }
+          req.flash("error", "Invalid email or password.")
+          res.redirect("/login")
+        })
+        .catch((err) => {
+          console.log(err)
+          res.redirect("/login")
+        })
+    })
+    .catch((err) => console.log(err))
+}
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email
+  const password = req.body.password
+  const errors = validationResult(req)
+  console.log(errors.array()[0])
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg,
+      oldInput: { email, password, confirmPassword: req.body.confirmPassword },
+    })
+  }
+
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] },
+      })
+      return user.save()
+    })
+    .then((result) => {
+      res.redirect("/login")
+      return transport.sendMail({
+        to: email,
+        from: "beqarioni@yahoo.co.uk",
+        subject: "Signup succeeded!",
+        html: "<h1>You successfully signed up!</h1>",
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err)
+    res.redirect("/")
+  })
+}
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash("error")
+  if (message.length > 0) {
+    message = message[0]
+  } else {
+    message = null
+  }
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    errorMessage: message,
+  })
+}
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err)
+      return res.redirect("/reset")
+    }
+    const token = buffer.toString("hex")
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash("error", "No account with that email found.")
+          return res.redirect("/reset")
+        }
+        user.resetToken = token
+        user.resetTokenExpiration = Date.now() + 3600000
+        return user.save()
+      })
+      .then((result) => {
+        res.redirect("/")
+        transport.sendMail({
+          to: req.body.email,
+          from: "shop@node-complete.com",
+          subject: "Password reset",
+          html: `
+            <p>You requested a password reset</p>
+            <p>Click this <a href="http://localhost:4000/reset/${token}">link</a> to set a new password.</p>
+          `,
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  })
+}
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then((user) => {
+      let message = req.flash("error")
+      if (message.length > 0) {
+        message = message[0]
+      } else {
+        message = null
+      }
+      res.render("auth/new-password", {
+        path: "/new-password",
+        pageTitle: "New Password",
+        errorMessage: message,
+        userId: user._id.toString(),
+        passwordToken: token,
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password
+  const userId = req.body.userId
+  const passwordToken = req.body.passwordToken
+  let resetUser
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      resetUser = user
+      return bcrypt.hash(newPassword, 12)
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword
+      resetUser.resetToken = undefined
+      resetUser.resetTokenExpiration = undefined
+      return resetUser.save()
+    })
+    .then((result) => {
+      res.redirect("/login")
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
